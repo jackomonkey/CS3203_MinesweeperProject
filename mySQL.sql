faab21c8ebc4a55b2c473925abcbfbd02338752f
Tanner Benbrook <98106348+TannerBenbroo@users.noreply.github.com>
2023-04-07 18:06:07 -0500

    Update mySQL.sql

--- mySQL.sql
+++ mySQL.sql
@@ -1,3 +1,3 @@
-CREATE TABLE Players(username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL);
-INSERT INTO Players(username, password) VALUES ('test username', 'test password);
-SELECT * FROM Players;
+CREATE TABLE Players(username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL); --this creates the table for our game with the given columns 'username' and 'password';
+INSERT INTO Players(username, password) VALUES ('test username', 'test password'); --this inserts a row into the table
+SELECT * FROM Players; --this can give you a visual representation of the current data in the table
