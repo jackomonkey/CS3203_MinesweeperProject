0f77b10c7f76d391781c8dad9cd771cb63c53ac5
Tanner Benbrook <98106348+TannerBenbroo@users.noreply.github.com>
2023-04-07 18:03:35 -0500

    Create mySQL.sql

---
+++ mySQL.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Players(username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL);
+INSERT INTO Players(username, password) VALUES ('test username', 'test password);
+SELECT * FROM Players;
