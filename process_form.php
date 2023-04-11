93fce18aee10a1cb761a0b2e20887f102794b38e
Tanner Benbrook <98106348+TannerBenbroo@users.noreply.github.com>
2023-04-11 12:24:30 -0500

    Update process_form.php

---
+++ process_form.php
@@ -0,0 +1,35 @@
+<?php
+// Connect to the MySQL database
+$servername = "localhost";
+$username = "u351964368_root";
+$password = "SoftwareEngineering3203";
+$dbname = "u351964368_SoftwareEngr";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Retrieve data submitted from the username and password fields
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Validate and sanitize the data
+$username = mysqli_real_escape_string($conn, $username);
+$password = mysqli_real_escape_string($conn, $password);
+
+// Insert the data into the specific table in the database
+$sql = "INSERT INTO Players (username, password) VALUES ('$username', '$password')";
+
+if (mysqli_query($conn, $sql)) {
+  //if connection works, cdir to random.php
+  include 'random.php';
+} else {
+  echo "Error inserting data: " . mysqli_error($conn);
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
