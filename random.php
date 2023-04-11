71a42cc0dc6112b477820a149c34d8189e4065e8
Tanner Benbrook <98106348+TannerBenbroo@users.noreply.github.com>
2023-04-11 12:25:16 -0500

    Update random.php

---
+++ random.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Big Red Button</title>
+	<style>
+		.button {
+			background-color: red;
+			color: white;
+			font-size: 36px;
+			padding: 20px 40px;
+			border: none;
+			border-radius: 10px;
+			cursor: pointer;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+	</style>
+</head>
+<body>
+	<button class="button">PRESS ME</button>
+</body>
+</html>
