864b6000017112d6093359f5d9ee8ac625102860
Tanner Benbrook <98106348+TannerBenbroo@users.noreply.github.com>
2023-04-19 16:36:49 -0500

    Update mySQL.sql

--- mySQL.sql
+++ mySQL.sql
@@ -1,3 +1,4 @@
-CREATE TABLE Players(username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL); --this creates the table for our game with the given columns 'username' and 'password';
-INSERT INTO Players(username, password) VALUES ('test username', 'test password'); --this inserts a row into the table
-SELECT * FROM Players; --this can give you a visual representation of the current data in the table
+CREATE TABLE Users(username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, time INT); --this creates the table for our game with the given columns 'username', 'password', and a time slot;
+INSERT INTO Players(username, password) VALUES ('test username', 'test password', 0); --this inserts a row into the table
+SELECT * FROM Users; --this can give you a visual representation of the current data in the table
+SELECT time FROM Users WHERE username = '$username' AND password = '$password' --selects the time of the given row where 'username' and 'password' are found
